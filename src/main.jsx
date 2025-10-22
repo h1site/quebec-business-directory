ee4a6fa417a0a5c3eb1c207e3922779845e8c252
h1site <info@h1site.com>
2025-10-21 20:20:44 -0400

    Revert "COMPLETE REWRITE: Dedicated mobile CSS for home page (NUCLEAR FIX)"
    This reverts commit 36e853ce024e7cb332810605e1a4b2a5c95e5c5f.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/index.css';
 import './styles/global.css';
-import './styles/home-mobile.css';
 import './services/i18n.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
