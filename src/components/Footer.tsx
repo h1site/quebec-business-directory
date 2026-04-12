ca6d85a86eeac61b7a4e5a6f2896b8c57d34f7fc
H1 Site <info@h1site.com>
2026-04-12 08:34:58 -0400

    fix: change community banner image to business meeting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
       <div className="relative overflow-hidden">
         <div
           className="absolute inset-0 bg-cover bg-center"
-          style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?w=1600&q=80)' }}
+          style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1557804506-669a67965ba0?w=1600&q=80)' }}
         />
         <div className="absolute inset-0 bg-white/85" />
         <div className="relative z-10 max-w-7xl mx-auto px-6 py-16">
